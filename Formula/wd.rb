9cd67e0526d263098ebcae51e78ffe07f48f050a
Kerekes Dániel <kerekes.daniel.23@icloud.com>
2026-02-20 01:19:16 +0100

    Fix sha256 checksums and workflow push permissions

--- Formula/wd.rb
+++ Formula/wd.rb
@@ -7,10 +7,10 @@ class Wd < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/kerdofficial/wd/releases/download/v#{version}/wd-macos-arm64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "fc315452489da647a0f6d776440ca289ce2796cc071aff0341c672d1cba7c999"
     else
       url "https://github.com/kerdofficial/wd/releases/download/v#{version}/wd-macos-x64.tar.gz"
-      sha256 "PLACEHOLDER"
+      sha256 "b9964cbcd687f77785292812df2bef63c1ebf3d00f34401813d86bb666b56e9f"
     end
   end
 
